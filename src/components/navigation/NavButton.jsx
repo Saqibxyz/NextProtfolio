c690c5c13c9a2bb190d785e58517e5f749fc3c27
Saqib Ayoub <saqibtelixy@gmail.com>
2024-05-08 18:43:03 +0530

    home responsive complete

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -10,6 +10,8 @@ import {
     FolderKanban,
 } from "lucide-react";
 import Link from "next/link";
+import ResponsiveComponent from "../ResponsiveComponent";
+import clsx from "clsx";
 
 const getIcon = (icon) => {
     switch (icon) {
@@ -40,35 +42,79 @@ const getIcon = (icon) => {
             return <Home className=" w-full h-auto " strokeWidth={1.5} />;
     }
 };
-const NavButton = ({ x, y, label, link, icon, newTab }) => {
+const NavButton = ({ x, y, label, link, icon, newTab,labelDirection="right" }) => {
     return (
 
 
+        <ResponsiveComponent>
+            {
+                ({ size }) => {
+                    return size && size >= 480 ?
 
 
-        <div
-            className=" absolute cursor-pointer z-50 "
-            style={{
-                transform: `translate(${x},${y})`,
-            }}
-        >
-            <Link
-                href={link}
-                target={newTab ? "_blank" : "_self"}
-                className=" text-foreground  rounded-full flex items-center justify-center custom-bg"
-                aria-label={label}
-                name={label}
-            >
-                <span className=" relative  w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent">
-                    {getIcon(icon)}
-                    <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
-                    <span className=" absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap group-hover:pause">
-                        {label}
-                    </span>
-                </span>
-            </Link>
-        </div>
 
+                        <div
+                            className=" absolute cursor-pointer z-50 "
+                            style={{
+                                transform: `translate(${x},${y})`,
+                            }}
+                        >
+                            <Link
+                                href={link}
+                                target={newTab ? "_blank" : "_self"}
+                                className=" text-foreground  rounded-full flex items-center justify-center custom-bg"
+                                aria-label={label}
+                                name={label}
+                            >
+                                <span className=" relative  w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent">
+                                    {getIcon(icon)}
+                                    <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
+                                    <span className=" absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap group-hover:pause ">
+                                        {label}
+                                    </span>
+                                </span>
+                            </Link>
+                        </div>
+
+
+                        :
+                        //Modify this
+                        <div
+                            className="w-fit cursor-pointer z-50 "
+
+                        >
+                            <Link
+                                href={link}
+                                target={newTab ? "_blank" : "_self"}
+                                className=" text-foreground  rounded-full flex items-center justify-center custom-bg"
+                                aria-label={label}
+                                name={label}
+                            >
+                                <span className=" relative  w-10 h-10 xs:w-14 xs:h-14 p-2.5 xs:p-4   hover:text-accent">
+                                    {getIcon(icon)}
+                                    <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
+                                    <span className={clsx(" absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap",labelDirection==="left"?"right-full left-auto":"")}>
+                                        {label}
+                                    </span>
+                                </span>
+                            </Link>
+                        </div>
+
+
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+        </ResponsiveComponent>
 
 
 
--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -3,6 +3,7 @@ import { BtnList } from "@/app/data";
 import React from "react";
 import NavButton from "./NavButton";
 import useScreenSize from "../hooks/useScreenSize";
+import ResponsiveComponent from "../ResponsiveComponent";
 
 
 const Navigation = () => {
@@ -13,37 +14,58 @@ const Navigation = () => {
 
     return (
         <div className="fixed h-screen flex items-center justify-center w-full ">
+            <ResponsiveComponent>
+                {({ size }) => {
+                    return size && size >= 480 ?
+                        <div className="w-max flex items-center justify-center relative hover:pause animate-spin-slow group">
+                            {BtnList.map((btn, index) => {
+                                const angleRadian = (index * angleIncrement * Math.PI) / 180;
 
 
-
-            <div className="w-max flex items-center justify-center relative hover:pause animate-spin-slow group">
-                {BtnList.map((btn, index) => {
-                    const angleRadian = (index * angleIncrement * Math.PI) / 180;
-
-
-                    const radius = isLarge ? `calc(20vw - 1rem)` : isMedium ? `calc(30vw - 1rem)` : `calc(40vw - 1rem)`
-
-
-
-
-                    const x = `calc(${radius}*${Math.cos(angleRadian)})`;
-                    const y = `calc(${radius}*${Math.sin(angleRadian)})`;
-
-                    return <NavButton key={btn.label} {...btn} x={x} y={y} />;
-                })}
-            </div>
+                                const radius = isLarge ? `calc(20vw - 1rem)` : isMedium ? `calc(30vw - 1rem)` : `calc(40vw - 1rem)`
 
 
 
 
+                                const x = `calc(${radius}*${Math.cos(angleRadian)})`;
+                                const y = `calc(${radius}*${Math.sin(angleRadian)})`;
+
+                                return <NavButton key={btn.label} {...btn} x={x} y={y} />;
+                            })}
+                        </div>
+                        :
+                        // Here starts the mobile version
+
+                        (
+                            <>
+                                <div className="w-full px-2.5 xs:p-0 space-y-4  xs:w-max flex flex-col items-start xs:items-center justify-center relative  group">
+                                    {BtnList.slice(0, BtnList.length / 2).map((btn, index) => {
+                                       
+
+                                        return <NavButton key={btn.label} {...btn}  x={0} y={0} />;
+                                    })}
+                                </div>
+                                <div className="w-full px-2.5 xs:p-0 space-y-4  xs:w-max flex flex-col items-end xs:items-center justify-center relative  group">
+                                    {BtnList.slice(BtnList.length / 2, BtnList.length).map((btn, index) => {
+                                       
+
+                                        return <NavButton key={btn.label} {...btn} labelDirection="left" x={0} y={0} />;
+                                    })}
+                                </div>
+                            </>
+                        )
 
 
 
 
 
 
+                }}
 
 
+
+
+            </ResponsiveComponent>
 
 
         </div>
